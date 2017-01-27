f2f4b80cb145910bd662ee4d83d4a4dd3edd6f94
Taldops <2dobert@informatik.uni-hamburg.de>
2017-01-27 21:28:22 +0100

    Quality, mute and fps options work now

--- unity-project/Assets/Scripts/FPSDisplay.cs
+++ unity-project/Assets/Scripts/FPSDisplay.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Author: Dave Hampson 
+ * Edited version of the script by Dave Hampson 
  * from http://wiki.unity3d.com/index.php/FramesPerSecond
  */
 
@@ -8,7 +8,14 @@ using System.Collections;
 
 public class FPSDisplay : MonoBehaviour
 {
+	public float size;
+
 	float deltaTime = 0.0f;
+
+	void Start()
+	{
+		size = Mathf.Max(size, 1);
+	}
 
 	void Update()
 	{
@@ -17,17 +24,19 @@ public class FPSDisplay : MonoBehaviour
 
 	void OnGUI()
 	{
+		//int w = Mathf.RoundToInt(Screen.width * size), h = Mathf.RoundToInt(Screen.height * size);
 		int w = Screen.width, h = Screen.height;
 
 		GUIStyle style = new GUIStyle();
 
 		Rect rect = new Rect(0, 0, w, h * 2 / 100);
-		style.alignment = TextAnchor.UpperLeft;
-		style.fontSize = h * 2 / 100;
-		style.normal.textColor = new Color (0.0f, 0.0f, 0.5f, 1.0f);
+		style.alignment = TextAnchor.UpperRight;
+		style.fontSize = Mathf.RoundToInt(size * h * 0.02f);
+		style.normal.textColor = new Color (0.2f, 9.0f, 0.2f, 1.0f);
 		float msec = deltaTime * 1000.0f;
 		float fps = 1.0f / deltaTime;
 		string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
 		GUI.Label(rect, text, style);
 	}
+			
 }
---
+++ unity-project/Assets/Scripts/SettingsPanel.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class SettingsPanel : MonoBehaviour {
+
+	public Dropdown qualityDD;
+	public Toggle muteToggle;
+	public Toggle FPSToggle;
+	//public AudioListener Listener;
+
+	// Use this for initialization
+	void Start () {
+		qualityDD.ClearOptions();
+		List<Dropdown.OptionData> options = new List<Dropdown.OptionData>();
+		foreach(string setting in QualitySettings.names)
+		{
+			options.Add(new Dropdown.OptionData(setting));
+		}
+		qualityDD.AddOptions(options);
+		qualityDD.value = QualitySettings.GetQualityLevel();
+	}
+
+	public void UpdateQualitySelection()
+	{
+		QualitySettings.SetQualityLevel(qualityDD.value, true);
+	}
+
+	public void ToggleMute()
+	{
+		AudioListener.pause = muteToggle.isOn;
+	}
+
+	public void ToggleFPS()
+	{
+		GameObject.Find("Canvas").GetComponent<FPSDisplay>().enabled = FPSToggle.isOn;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
